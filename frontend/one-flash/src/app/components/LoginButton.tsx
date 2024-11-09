20afca9d6ad185935213d5c6fbf624e0c49f9772
johnhenrywalker <jhw74912@uga.edu>
2024-11-09 17:14:35 -0500

    LoginButton created
    non-functional

---
+++ frontend/one-flash/src/app/components/LoginButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LoginButton() {
+  return (
+   <button className="LoginButton">Log In</button>
+  );
+}
+
+export default LoginButton;
--- frontend/one-flash/src/app/page.tsx
+++ frontend/one-flash/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import RegisterButton from "./components/RegisterButton";
+import LoginButton from "./components/LoginButton";
 
 export default function Home() {
   return (
     <>
       <RegisterButton />
+      <LoginButton />
     </>
   );
 }
